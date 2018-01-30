f3125a90cce1a1f88d22cf20cc64bf141540e2cb
unite <zhao.terryy@gmail.com>
2018-01-30 12:58:55 -0500

    Initial commit

---
+++ Source/Entropy/EntropyGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "EntropyGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ENTROPY_API AEntropyGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
