afaeae227d4c1eaed3272730ddb43ffbb4e136e6
Darren Yam <Darren Yam@DESKTOP-UNA8P2A>
2018-02-03 20:50:23 -0500

    For UI

--- Source/Entropy/EntropyGameModeBase.cpp
+++ Source/Entropy/EntropyGameModeBase.cpp
@@ -1,7 +1,30 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
+#include "Entropy.h"
 #include "EntropyGameModeBase.h"
 
+void AEntropyGameModeBase::BeginPlay()
+{
+	Super::BeginPlay();
+	ChangeMenuWidget(StartingWidgetClass);
+}
 
+void AEntropyGameModeBase::ChangeMenuWidget(TSubclassOf<UUserWidget> NewWidgetClass) 
+{
+	if (CurrentWidget != nullptr)
+	{
+		CurrentWidget->RemoveFromViewport();
+		CurrentWidget = nullptr;
+	}
+
+	if (NewWidgetClass != nullptr)
+	{
+		CurrentWidget = CreateWidget<UUserWidget>(GetWorld(), NewWidgetClass);
+		if (CurrentWidget != nullptr)
+		{
+			CurrentWidget->AddToViewport();
+		}
+	}
+}
 
 
--- Source/Entropy/EntropyGameModeBase.h
+++ Source/Entropy/EntropyGameModeBase.h
@@ -3,18 +3,30 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
 #include "GameFramework/GameModeBase.h"
 #include "EntropyGameModeBase.generated.h"
 
 /**
  * 
  */
-UCLASS()
+UCLASS(abstract)
 class ENTROPY_API AEntropyGameModeBase : public AGameModeBase
 {
 	GENERATED_BODY()
 	
-	
-	
-	
+public:
+	/** Remove the current menu widget and create a new one from the specified class, if provided. */
+	UFUNCTION(BlueprintCallable, Category = "Entropy")
+	void ChangeMenuWidget(TSubclassOf<UUserWidget> NewWidgetClass);
+
+protected:
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Entropy")
+	TSubclassOf<UUserWidget> StartingWidgetClass;
+
+	/** The widget instance that we are using as our menu. */
+	UPROPERTY()
+	UUserWidget* CurrentWidget;	
 };
