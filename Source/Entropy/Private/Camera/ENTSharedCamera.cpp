5692a9be71eaee5bf54814607e81ec94a046fccb
Darren Yam <Darren Yam@DESKTOP-UNA8P2A>
2018-02-07 12:02:02 -0500

    test

---
+++ Source/Entropy/Private/Camera/ENTSharedCamera.cpp
@@ -0,0 +1,54 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ENTSharedCamera.h"
+#include "Camera/CameraComponent.h"
+#include "EngineUtils.h"
+#include "ENTCharacter.h"
+#include "GameFramework/SpringArmComponent.h"
+
+
+// Sets default values
+AENTSharedCamera::AENTSharedCamera()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	RootComp = CreateDefaultSubobject<USceneComponent>("Root");
+	RootComponent = RootComp;
+
+	CameraBoomComp = CreateDefaultSubobject<USpringArmComponent>("Camera Boom");
+	CameraBoomComp->SetupAttachment(RootComp);
+
+	CameraComp = CreateDefaultSubobject<UCameraComponent>("Camera");
+	CameraComp->SetupAttachment(CameraBoomComp, USpringArmComponent::SocketName);
+}
+
+// Called when the game starts or when spawned
+void AENTSharedCamera::BeginPlay()
+{
+	Super::BeginPlay();
+	for (TActorIterator<AENTCharacter> Itr(GetWorld()); Itr; ++Itr)
+	{
+		Players.Add(*Itr);
+	}
+}
+
+FVector AENTSharedCamera::CalcCameraPosition()
+{
+	FVector CameraLoc;
+	for (const AActor* Player : Players)
+	{
+		CameraLoc += Player->GetActorLocation();
+	}
+	
+	CameraLoc /= Players.Num();
+	return CameraLoc;
+}
+
+// Called every frame
+void AENTSharedCamera::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	SetActorLocation(CalcCameraPosition());
+}
---
+++ Source/Entropy/Private/Player/ENTPlayerCameraManager.cpp
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ENTPlayerCameraManager.h"
+#include "EntropyGameModeBase.h"
+#include "ENTSharedCamera.h"
+#include "../../../../../../../UE4_UWP/Engine/Source/Runtime/Engine/Classes/Camera/CameraComponent.h"
+
+void AENTPlayerCameraManager::BeginPlay()
+{
+	AEntropyGameModeBase* const GM = (GetWorld() != nullptr) ? GetWorld()->GetAuthGameMode<AEntropyGameModeBase>() : nullptr;
+
+	if (GM)
+	{
+		SharedCamera = GM->GetSharedCamera();
+	}
+}
+
+void AENTPlayerCameraManager::UpdateCamera(float DeltaTime)
+{
+	Super::UpdateCamera(DeltaTime);
+}
+
+void AENTPlayerCameraManager::UpdateViewTarget(FTViewTarget& OutVT, float DeltaTime)
+{
+	if ((PendingViewTarget.Target != NULL) && BlendParams.bLockOutgoing && OutVT.Equal(ViewTarget))
+	{
+		return;
+	}
+
+	if (SharedCamera)
+	{
+		FMinimalViewInfo OrigPOV = OutVT.POV;
+
+		SharedCamera->GetCameraComponent()->GetCameraView(DeltaTime, OutVT.POV);
+
+		OutVT.POV.PostProcessSettings.SetBaseValues();
+		OutVT.POV.PostProcessBlendWeight = 1.0f;
+	}
+	else
+	{
+		Super::UpdateViewTarget(OutVT, DeltaTime);
+	}
+}
