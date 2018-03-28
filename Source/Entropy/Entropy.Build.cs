bc65aef5273e8bb03fad22da7964156e719ad753
unite <zhao.terryy@gmail.com>
2018-03-28 01:19:14 -0400

    gamertag debug message added

--- Source/Entropy/Entropy.Build.cs
+++ Source/Entropy/Entropy.Build.cs
@@ -8,7 +8,7 @@ public class Entropy : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "AIModule", "Paper2D" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "AIModule", "Paper2D", "OnlineSubsystem", "OnlineSubsystemUtils" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
